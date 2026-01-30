0c85b191b9517909f30828058f8bafa427fa8b0e
harihara-1869 <harihara1869@gmail.com>
2026-01-30 16:08:42 +0530

    Create update-profile pic route

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from "../models/user.model.js";
 import bcrypt from "bcryptjs";
 import { generateToken } from "../lib/utils.js";
+import cloudinary from "../lib/cloudinary.js";
 
 export const signup = async (req, res) => {
   const { fullName, email, password } = req.body;
@@ -95,6 +96,24 @@ export const logout = (req, res) => {
 };
 
 export const updateProfile = async (req, res) => {
-  // Implementation for updating user profile goes here
-  res.send("Got in to update profile")
+  try {
+    const { profilePic } = req.body;
+    const userId = req.user._id;
+
+    if (!profilePic) {
+      return res.status(400).json({ message: "Profile pic is required" });
+    }
+
+    const uploadResponse = await cloudinary.uploader.upload(profilePic);
+    const updateUser = await User.findByIdAndUpdate(
+      userId,
+      { profilePic: uploadResponse.secure_url },
+      { new: true },
+    );
+    res.status(200).json(updateUser);
+  } catch (error) {
+    console.log("error in update profile:", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
 };
+
--- backend/src/middleware/auth.middleware.js
+++ backend/src/middleware/auth.middleware.js
@@ -18,7 +18,7 @@ export const protectRoute = async (req, res, next) => {
       return res.status(401).json({ message: 'Token is not valid.' });
     }
 
-    const user = await User.findById(decoded.userId).select("-password")
+    const user = await User.findById(decoded.id).select("-password")
 
     if(!user){
       return res.status(404).json({message: "User not found"})
