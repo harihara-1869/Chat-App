e546660ff3c7c00ec264cf19b1cf349847e5cce9
harihara-1869 <harihara1869@gmail.com>
2026-01-31 12:55:06 +0530

    Open to local network for testing

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,10 +2,10 @@ import express from 'express';
 import authRoutes from './routes/auth.route.js';
 import messageRoutes from './routes/message.route.js';
 import dotenv from 'dotenv';
-import {initDB} from './lib/db.js';
+import { initDB } from './lib/db.js';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
-import {app, server} from "./lib/socket.js"
+import { app, server } from "./lib/socket.js"
 
 dotenv.config();
 
@@ -15,7 +15,7 @@ const PORT = process.env.PORT || 5001;
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-  origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+  origin: true,
   credentials: true,
 }));
 
@@ -29,7 +29,7 @@ async function startServer() {
     console.log("MongoDB connected");
 
     // 2️⃣ Start server ONLY after DB is ready
-    server.listen(PORT, () => {
+    server.listen(PORT, "0.0.0.0", () => {
       console.log(`Server running on port ${PORT}`);
     });
   } catch (error) {
